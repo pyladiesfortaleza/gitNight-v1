1e210ad2677d0fc6f8e19a78ce75b54e1fa7612a
Jessyka Lage <jessykalage@gmail.com>
2018-10-25 19:58:55 -0300

    Desafio: Calcula o MMC

--- challenges/mmc.py
+++ challenges/mmc.py
@@ -1,6 +1,6 @@
 """
 Criado por: @gabrielabezerra
-Resolvido por:
+Resolvido por: @jessyka
 
 Crie um programa que calcula o MMC de dois números naturais.
 
@@ -11,3 +11,18 @@ MMC = 110
 
 
 """
+
+num1 = int(input("Digite um número inteiro:"))
+num2 = int(input("Digite outro número inteiro:"))
+
+if num1 > num2:
+    maior = num1
+else:
+    maior = num2
+
+for i in range(maior):
+    aux = num1 * i
+    if (aux % num2) == 0:
+        mmc = aux
+
+print(mmc)
